51cdd96667d6ae6a6515950d176850128e31fba1
Davidai-liangzi <1696889557@qq.com>
2025-10-14 13:23:47 +0800

    Delete 创建随机数组(整型）.py

--- 创建随机数组(整型）.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-n1=np.random.randint(1,100,20)    #min and max and the num
-print(n1)
-n2=np.random.randint(1,20)    # 空集
-print(n2)
-n3=np.random.randint(5,size=(2,3))    #2维
-print(n3)
