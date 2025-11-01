1e1fb82685b8138d71633d4cf5776884023d6921
Davidai-liangzi <1696889557@qq.com>
2025-11-01 09:27:47 +0800

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,14 @@
+#include<opencv2/opencv.hpp>
+
+int main() {
+    cv::Mat img=cv::imread("C:/Users/16968/Desktop/123/bed_pic.png");
+    cv::Mat hsv;
+    cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV);
+    cv::inRange(hsv,cv::Scalar(150,50,50),cv::Scalar(180,255,255),hsv);
+    cv::Mat result;
+    cv::bitwise_or(img,img,result,hsv);
+    cv::imshow("result",result);
+    cv::waitKey();
+    cv::destroyAllWindows();
+    return 0;
+}
